86c5cf5e62a4ccd636775eae96c14144205648bd
gmilev09 <gmilev09@gmail.com>
2026-03-15 11:23:26 +0200

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -10,7 +10,7 @@ export const shopProducts = [
   },
   {
     id: "2",
-    name: "LED/UV лампа 4",
+    name: "LED/UV лампа",
     price: 27.9,
     category: "електроуреди",
     in_stock: true,
@@ -46,7 +46,268 @@ export const shopProducts = [
     in_stock: true,
     image_url: "https://i.postimg.cc/rpZNtvLg/Ekranna-snimka-2026-03-01-203954.png",
     description: ""
-  }
+  },
+   {
+    id: "6",
+    name: "UV/LED фенер",
+    price: 6.9,
+    category: "електроуреди",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/fRn91MwW/Ekranna-snimka-2026-03-01-210936.png",
+    description: ""
+  },
+   {
+    id: "7",
+    name: "Професионална електрическа пила",
+    price: 150,
+    category: "електроуреди",
+    in_stock: true,
+    image_url: "https://ae01.alicdn.com/kf/Se3a4224d204546dbac1af78385d2a6fcP.jpg?width=800&height=800&hash=1600",
+    description: ""
+  },
+  {
+    id: "8",
+    name: "Горни форми за изграждане",
+    price: 8.5,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/hjqTxgYg/Ekranna-snimka-2026-03-01-205431.png",
+    description: "Горни форми за изграждане - 120бр."
+      },
+   {
+    id: "9",
+    name: "Фибро стъкло ",
+    price: 1.49,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/Hk9gHtC8/Ekranna-snimka-2026-03-01-204912.png",
+    description: "Фибро стъкло за лепене на счупени нокти"
+  },
+   {
+    id: "10",
+    name: "Органаизер за фрези с четка за почистване",
+    price: 4.5,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/T3vSKX32/Ekranna-snimka-2026-03-01-201437.png", image_url_2: "https://i.postimg.cc/xd04PyHY/Ekranna-snimka-2026-03-01-201459.png",
+    description: "С капацитет за 30 накраиника"
+  },
+   {
+    id: "11",
+    name: "Огледален пигмент - 6 цвята",
+    price: 8.9,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/Nfs2BK17/Ekranna-snimka-2026-03-01-201117.png", image_url_2:"https://i.postimg.cc/sXP1Rzxw/Ekranna-snimka-2026-03-01-201138.png",
+    description: ""
+  },
+   {
+    id: "12",
+    name: "Хартиени форми за изграждане",
+    price: 7.9,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/63dPtjdX/Ekranna-snimka-2026-03-01-192942.png", image_url_2:"https://i.postimg.cc/GhM5Lssk/Ekranna-snimka-2026-03-01-193034.png", image_url_3:"https://i.postimg.cc/4xq2tk5W/Ekranna-snimka-2026-03-01-193000.png",
+    description: "100бр."
+  },
+   {
+    id: "13",
+    name: "Розово фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/MKZ7pPy3/471333708-615367804306499-3080603529564129633-n.jpg",
+    description: ""
+  },
+   {
+    id: "14",
+    name: "Златно фолио",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/gJY97bZZ/471568956-615366800973266-4051339064563387387-n.jpg",
+    description: ""
+  },
+   {
+    id: "15",
+    name: "Сребърно фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/PqDrHHJV/471695978-615366910973255-3947372836038095415-n.jpg",
+    description: ""
+  },
+   {
+    id: "16",
+    name: "Розово-златно фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/m2TcSPkF/471756113-615366804306599-6347641468749741882-n.jpg",
+    description: ""
+  },
+   {
+    id: "17",
+    name: "Златно фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/8zKrcTTY/471872643-615368094306470-1997504274679167585-n.jpg",
+    description: ""
+  },
+   {
+    id: "18",
+    name: "Синьо фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/dtcRcQ06/471682639-615366787639934-5173947010396223195-n.jpg",
+    description: ""
+  },
+   {
+    id: "19",
+    name: "Червено фолио ",
+    price: 1.99,
+    category: "аксесоари",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/rpMW7DZd/471850100-615367930973153-3910663256230308150-n.jpg",
+    description: ""
+  },
+   {
+    id: "20",
+    name: "Четка за почистване на фрези",
+    price: 2.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/Z5N7msPZ/Ekranna-snimka-2026-03-01-200520.png",
+    description: ""
+  },
+   {
+    id: "21",
+    name: "Четка за гел",
+    price: 6.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/k4RdPv5Y/Ekranna-snimka-2026-03-01-193957.png",
+    description: "Четка - котешко езиче за гел и акрил"
+  },
+   {
+    id: "22",
+    name: "Тънка четка за декорации",
+    price: 6.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/3w7hpv9q/Ekranna-snimka-2026-03-01-194625.png",
+    description: "Хамелеонова четка за рисъ=уване - 9мм"
+  },
+   {
+    id: "23",
+    name: "Тънка четка за декорации",
+    price: 6.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/8kb6rR4T/Ekranna-snimka-2026-03-01-193957.png",
+    description: "Хамелеонова четка за рисуване - 11мм"
+  },
+   {
+    id: "24",
+    name: "Гъбички за омбре с щипка",
+    price: 4.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/TPhyzBv1/Ekranna-snimka-2026-03-01-193651.png",image_url_2:"https://i.postimg.cc/ht1bDkYT/Ekranna-snimka-2026-03-01-193034.png",image_url_3: "https://i.postimg.cc/650yyB9v/Ekranna-snimka-2026-03-01-193358.png",
+    description: ""
+  },
+   {
+    id: "25",
+    name: "Четка за декорации и гел",
+    price: 6.9,
+    category: "четки",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/g2zm5RbJ/Ekranna-snimka-2026-03-01-192716.png",
+    description: ""
+  },
+   {
+    id: "26",
+    name: "Златен резец за удължители",
+    price: 4.9,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/Gm9W0jts/Ekranna-snimka-2026-03-01-202958.png",
+    description: ""
+  },
+   {
+    id: "27",
+    name: "Гелотина ",
+    price: 4.5,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/VNW5x1dj/Ekranna-snimka-2026-03-01-202448.png",image_url_2: "https://i.postimg.cc/8c21Fn7D/Ekranna-snimka-2026-03-01-202528.png", image_url_3: "https://i.postimg.cc/xTmjrYZz/Ekranna-snimka-2026-03-01-202502.png",
+    description: "Гелотина от неръждаема стомана за скъсяване на нокти и удължители"
+  },
+   {
+    id: "28",
+    name: "Силиконов полировчик",
+    price: 2.9,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/YqMD7znQ/Ekranna-snimka-2026-03-01-202049.png",
+    description: ""
+  },
+   {
+    id: "29",
+    name: "Буфер блок пила",
+    price: 1.5,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/J7bzyR15/Ekranna-snimka-2026-03-01-200004.png",
+    description: ""
+  },
+   {
+    id: "30",
+    name: "Керамичен накраиник за електрическа пила с червена насечка",
+    price: 9.9,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/xTJNpwYJ/Ekranna-snimka-2026-03-01-195555.png",
+    description: ""
+  },
+   {
+    id: "31",
+    name: "Керамичен накраиник за електричвска пила с жълта насечка",
+    price: 9.9,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/N011JW6R/Ekranna-snimka-2026-03-01-195609.png",
+    description: ""
+  },
+   {
+    id: "32",
+    name: "Избутвач ",
+    price: 2.9,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://i.postimg.cc/59XhXgbt/Ekranna-snimka-2026-03-01-193957.png",image_url_2: "https://i.postimg.cc/BQxwFwyX/Ekranna-snimka-2026-03-01-194209.png",image_url_3: "https://i.postimg.cc/QMn6sLB0/Ekranna-snimka-2026-03-01-194019.png",
+    description: "Избутвач от неръждаема стомана"
+  },
+   {
+    id: "33",
+    name: "Хартиени пили 100/180",
+    price: 0.99,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://ae01.alicdn.com/kf/Se2812b34e81b4c029328cd5b4ebb7e44j.jpg",
+    description: ""
+  },
+   {
+    id: "34",
+    name: "Дървени пили за маникюр 100/180",
+    price: 0.99,
+    category: "инструменти и пили",
+    in_stock: true,
+    image_url: "https://ae01.alicdn.com/kf/S378316ffa374451abeb161596780849fA.jpg?width=900&height=900&hash=1800",
+    description: ""
+  },
 ];
 
 export const getProductById = (id) => shopProducts.find((product) => product.id === String(id));
