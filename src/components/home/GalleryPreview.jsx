cfec15f92907a8d291e9dfc22c6f78d486dd01d8
gmilev09 <gmilev09@gmail.com>
2026-04-23 15:43:57 +0300

    Update GalleryPreview.jsx

--- src/components/home/GalleryPreview.jsx
+++ src/components/home/GalleryPreview.jsx
@@ -3,10 +3,10 @@ import { Button } from "../ui/button";
 import { ArrowRight, Images } from "lucide-react";
 import { Link } from "react-router-dom";
 
-import previewImg1 from "../../NailsAcademy/Gallery/e1aaf6e9-032d-456a-97df-636ec298749f.jpg";
+import previewImg4 from "../../NailsAcademy/Gallery/e1aaf6e9-032d-456a-97df-636ec298749f.jpg";
 import previewImg2 from "../../NailsAcademy/Gallery/587504285_18048329723675195_6197362676033051046_n.jpg";
 import previewImg3 from "../../NailsAcademy/Gallery/a65b7d91-e3b1-42ba-a98e-7184a22c878b.jpg";
-import previewImg4 from "../../NailsAcademy/Gallery/viber_image_2026-04-23_13-40-57-131.jpg";
+import previewImg1 from "../../NailsAcademy/Gallery/viber_image_2026-04-23_13-40-57-131.jpg";
 import previewImg5 from "../../NailsAcademy/Gallery/viber_image_2026-04-23_13-38-29-918.jpg";
 
 const galleryImages = [
